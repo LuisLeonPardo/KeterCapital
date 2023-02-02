beaf7c4b4859763d7bc36bafacb3eb1a56236a79
LOLE81 <martinezpm@gmail.com>
2023-02-02 18:25:10 -0300

    adds styles in Home component, mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,8 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
-      backgroundImage: {},
-      colors: {
-        yellow: "#FDA300",
-        redCircle: "#FF3232",
-        orangeCircle: "#FFB932",
-        greenCircle: "#80FF32",
-        lightBlueCircle: "#32CEFF",
-        golden: "#DD8F00",
+      backgroundImage: {
+        men: "url('/men.png')",
       },
     },
   },
