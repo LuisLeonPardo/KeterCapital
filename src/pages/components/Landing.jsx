b8f04cb26e70b8482eb65cd3e407bbc07380a951
LOLE81 <martinezpm@gmail.com>
2023-02-03 15:22:29 -0300

    creates Investments component, adds styles and mobile responsive

--- src/pages/components/Landing.jsx
+++ src/pages/components/Landing.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./NavBar/NavBar";
-import Home from "./Home.jsx";
+import Home from "./Home/Home.jsx";
+import Investments from "./Investments/Investments";
 
 export default function Landing() {
   return (
-    <main className="bg-[#FAFAFA] h-screen flex flex-col items-center font-inter">
+    <main className="bg-[#FAFAFA] flex flex-col items-center font-inter">
       <Navbar />
       <Home />
-      <h1>Investments</h1>
+      <Investments />
       <h1>Catches</h1>
       <h1>Footer</h1>
     </main>
