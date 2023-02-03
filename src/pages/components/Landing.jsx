d9c439dfa423fac5d59bfc4cf03499b424cbd469
Lucas <luqasmagra@gmail.com>
2023-02-03 12:41:30 -0300

    Add responsive footer

--- src/pages/components/Landing.jsx
+++ src/pages/components/Landing.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Navbar from "./NavBar/NavBar";
 import Home from "./Home.jsx";
+import FooterDesktop from "./Footer/FooterDesktop";
+import FooterMobile from "./Footer/FooterMobile";
 
 export default function Landing() {
   return (
-    <main className="bg-[#FAFAFA] h-screen flex flex-col items-center font-inter">
+    <main className="bg-[#FAFAFA] flex flex-col items-center font-inter">
       <Navbar />
       <Home />
-      <h1>Investments</h1>
-      <h1>Catches</h1>
-      <h1>Footer</h1>
+      <footer className="w-full">
+        <div className="sm:hidden flex">
+          <FooterMobile />
+        </div>
+        <div className="sm:flex hidden ">
+          <FooterDesktop />
+        </div>
+      </footer>
     </main>
   );
 }
