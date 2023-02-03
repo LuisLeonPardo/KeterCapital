0636d12bd4c8ea0bfe5c22fe63dc4b6f28679bda
Lucas <luqasmagra@gmail.com>
2023-02-03 11:56:12 -0300

    Fix Head component

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,6 @@ export default function Home() {
           content="Keter Capital:`You dream it, we invest´"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <>
         <Landing />
